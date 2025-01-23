7d6264e46ff8744001fd43460125fb35208592e0
dbarania <baraniak.damian@gmail.com>
2025-01-23 12:09:05 +0100

    New, hopefuly final version of a car design, vision module updates

--- vehicle/raspberry_code/camera_module.py
+++
@@ -1,8 +0,0 @@
-from gpio_module import GpioModule
-
-
-class CameraModule(GpioModule):
-    def __init__(self, camera_id):
-        super().__init__()
-        self.camera = camera_id
-        self.h = hash(camera_id )
--- vehicle/raspberry_code/gpio_module.py
+++ vehicle/raspberry_code/gpio_module.py
@@ -7,8 +7,6 @@ class GpioModule:
             self.pi = pi
 
     def kill_self(self):
-        # filtered_values = [value for key, value in data.items() if keyword in key]
-        # return filtered_values
         if hasattr(self, "pi"):
             pins = [value for key, value in self.__dict__ if "pin" in key]
             for pin in pins:
