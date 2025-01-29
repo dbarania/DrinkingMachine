a981255ca4787135d71719357bee0de2db0744ff
dbarania <baraniak.damian@gmail.com>
2025-01-29 16:17:06 +0100

    Some weird alternative implementations

---
+++ vehicle/raspberry_code/reality_bending.py
@@ -0,0 +1,74 @@
+import robot2
+import curses
+
+
+#
+#
+# def main(stdscr):
+#     curses.cbreak()  # Reacts instantly to keypresses
+#     stdscr.keypad(True)  # Enables arrow key detection
+#     stdscr.clear()
+#     stdscr.refresh()
+#
+#     while True:
+#         key = stdscr.getch()
+#         stdscr.clear()  # Clear the screen before printing
+#         stdscr.addstr("Press WASD keys to move. Press Q to quit.\n")
+#
+#
+#         if key == ord('q'):
+#             break
+#         elif key == ord('w'):
+#             stdscr.addstr("Moving Up\n")
+#         elif key == ord('a'):
+#             stdscr.addstr("Moving Left\n")
+#         elif key == ord('s'):
+#             stdscr.addstr("Moving Down\n")
+#         elif key == ord('d'):
+#             stdscr.addstr("Moving Right\n")
+#
+#         stdscr.refresh()
+#
+#
+# curses.wrapper(main)
+
+class RealityBendingApplication:
+    def __init__(self):
+        self.robot = robot2.Robot(connection=False)
+        self.last_pressed = None
+
+    def main(self, stdscr):
+        curses.cbreak()  # Reacts instantly to keypresses
+        stdscr.keypad(True)  # Enables arrow key detection
+        stdscr.clear()
+        stdscr.refresh()
+
+        while True:
+            key = stdscr.getch()
+            stdscr.clear()  # Clear the screen before printing
+            stdscr.addstr("Press WASD keys to move. Press Q to quit.\n")
+            if key == self.last_pressed:
+                continue
+            else:
+                self.last_pressed = key
+            if key == 27:
+                break
+            elif key == ord('w'):
+                self.robot.motors_controller.move_straight()
+            elif key == ord('s'):
+                self.robot.motors_controller.move_straight(-1)
+            elif key == ord('a'):
+                self.robot.motors_controller.move_slight_left()
+            elif key == ord('d'):
+                self.robot.motors_controller.move_slight_right()
+            elif key == ord('q'):
+                self.robot.motors_controller.turn_in_place(1)
+
+            elif key == ord('e'):
+                self.robot.motors_controller.turn_in_place(-1)
+
+            stdscr.refresh()
+
+
+app = RealityBendingApplication()
+curses.wrapper(app.main)
